76ccabc1353dd3e18516a00708a39d7c8f8fd76d
AsemHub <asemalakabani@gmail.com>
2020-01-30 17:08:19 +0100

    update

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 // alert ('hello world')
-const Firs_tName = "Asem"
+const First_Name = "Asem"
 const Last_Name = "Alakabani"
 let fullName = First_Name + " " + Last_Name
+
+let myAddress = 'Strada Del Cantello, 14\n10138 Torino (TO)\nItaly'
+
+const Street = "Strada Del Cantello"
+const Street_Number = "14"
+const Cap = "10138"
+const City = "Torino"
+const province = "TO"
+const Nation = "Italy"
+let address = Street + ", " + Street_Number + "\n" + Cap + " " + City + " (" + province + ")\n" + Nation
+console.log(address + '\n' + myAddress)
