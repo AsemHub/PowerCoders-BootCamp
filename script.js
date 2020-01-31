46f936cf11a222cd80907e027d652104ad3d1559
AsemHub <asemalakabani@gmail.com>
2020-01-31 10:20:44 +0100

    update

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 // alert ('hello world')
-const First_Name = "Asem"
-const Last_Name = "Alakabani"
-let fullName = First_Name + " " + Last_Name
+const FirstName = "Asem"
+const LastName = "Alakabani"
+let fullName = FirstName + " " + LastName
 
 let myAddress = 'Strada Del Cantello, 14\n10138 Torino (TO)\nItaly'
 
 const Street = "Strada Del Cantello"
-const Street_Number = "14"
+const StreetNumber = "14"
 const Cap = "10138"
 const City = "Torino"
 const province = "TO"
 const Nation = "Italy"
-let address = Street + ", " + Street_Number + "\n" + Cap + " " + City + " (" + province + ")\n" + Nation
+let address = Street + ", " + StreetNumber + "\n" + Cap + " " + City + " (" + province + ")\n" + Nation
 console.log (address + '\n' + myAddress)
 let admin 
 let name ="Jhon"
 admin = "Jhon"
-alert (admin)
+//alert (admin)
 
-let newAddress =`${Street}, ${Street_Number}
+let newAddress =`${Street}, ${StreetNumber}
 ${Cap} ${City} (${province})
 ${Nation}`
 console.log(newAddress)
+
+const FName = 'First Name: Asem'
+const LName = 'Last Name Alakabani'
+const Nationality = 'Nationality: Syria'
+const Education = 'Education: Powercoders Bootcamp'
+const SoftSkills = 'Soft Skills: Survivor, HardWorker'
+const Languages = 'Languages: Arabic, English, Italian'
+let CV = `${FName} 
+${LName}
+${Nationality}
+${Education}
+${SoftSkills}
+${Languages}`
+console.log(CV)
