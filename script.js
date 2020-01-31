9dab2755adcd8b3b77cd2c4748fc460a0828b7d6
AsemHub <asemalakabani@gmail.com>
2020-01-31 09:13:23 +0100

    update

--- script.js
+++ script.js
@@ -12,4 +12,13 @@ const City = "Torino"
 const province = "TO"
 const Nation = "Italy"
 let address = Street + ", " + Street_Number + "\n" + Cap + " " + City + " (" + province + ")\n" + Nation
-console.log(address + '\n' + myAddress)
+console.log (address + '\n' + myAddress)
+let admin 
+let name ="Jhon"
+admin = "Jhon"
+alert (admin)
+
+let newAddress =`${Street}, ${Street_Number}
+${Cap} ${City} (${province})
+${Nation}`
+console.log(newAddress)
