b96b8ebf7aef9a6f32a83b0e29909523a780dae6
AsemHub <asemalakabani@gmail.com>
2020-02-06 11:31:43 +0100

    update

---
+++ loops.js
@@ -0,0 +1,50 @@
+// let i = 0
+// while ( i < 3 ) {
+// console.log(i)
+// i++;
+// }
+
+// let p = 3
+// while (p) {
+// console.log(p)
+// p--;
+// }
+
+
+// let i = 0
+// let string = ""
+// while (i < 16 ) {
+//     string += "Na"
+//     i++
+// }
+// console.log(`${string} ${'Batman!'}`)
+
+// let string = ""
+// for (let i = 0; i < 16; i++) {
+//     string += "Na"
+// }
+// console.log(`${string} ${'Batman!'}`)
+
+
+
+// let sum = ""
+// for (let i = 1; i<10; sum++){
+//     sum += "2" 
+// }console.log(sum)
+
+// let result = ""
+// let mult = result + 1
+// for (let i = 1; i < 10; i++) {
+//         result += (i*mult)
+// }console.log(result)
+
+// for (let i = prompt('give a number greater than 100'); i > 100;) {
+//         if (i<100) {
+//                 prompt('give a number greater than 100');
+// else {}
+//         }
+
+// let i = prompt('give a number greater than 100')
+// while (i & i <= 100) {
+//         i = prompt('please try again')
+// }
