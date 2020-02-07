84e7d376788619d1b06b33e873e87fc423848fea
AsemHub <asemalakabani@gmail.com>
2020-02-07 12:07:43 +0100

    update

--- loops.js
+++ loops.js
@@ -32,11 +32,6 @@
 //     sum += "2" 
 // }console.log(sum)
 
-// let result = ""
-// let mult = result + 1
-// for (let i = 1; i < 10; i++) {
-//         result += (i*mult)
-// }console.log(result)
 
 // for (let i = prompt('give a number greater than 100'); i > 100;) {
 //         if (i<100) {
@@ -48,3 +43,85 @@
 // while (i & i <= 100) {
 //         i = prompt('please try again')
 // }
+
+// for (let i = prompt('give a number greater than 100');
+// i&i <= 100;
+// i = prompt('please try again')) 
+// { }
+
+// let x=1;
+// let y=1;
+// const max=12;
+// for (let j = 1; j < max; j++) {
+
+//    let result = "";
+//    x=1;
+//         for (let i = 1; i < max; i++) {
+
+//                 result += (x + "x" + y + "=" + x * y + "|")
+//                 x++;
+//         }
+
+//     ++y;
+//    console.log(result);
+
+
+// for (let j=1; j<11; j++) {
+//         let result = ""
+//         for (let i = 1; i < 11; i++) {
+//                 result = `Table Of ${j} ${i}x${j} = ${i*j}`
+//                 console.log(`${result}`) }
+//         }
+
+// Table of 1: 1x1 = 1 1x2 = 2...
+// Table of 2: 2x1 = 1 2x2 = 4...
+
+// const ROWS = 3;
+// const COLUMNS = 7;
+// const PATTERN = '*';
+
+// let rectangle = '';
+// loops over rows
+// for (let i = 0; i < 3; i++) {
+//         let row = "";
+//         // loops over columns
+       
+       
+       
+//         for (let j = 0; j < 7; j++) {
+//                 row += "*";
+//         }
+//         // appends the newly created row to the rectangle
+//         if (i < 3 - 1) {
+//                 rectangle += row + '\n';
+//         } else {
+//                 rectangle += row;
+//         }
+
+
+
+
+
+// }
+// console.log(rectangle);
+
+// for (let j = 0; j < 10; j++) {
+      
+//         let str = ""
+
+//         for (let i = 0; i < 10; i++) {
+//                 str= str + "*";
+//         }
+//         console.log(str)
+
+// }
+
+for (let j=1; j<11; j++) {
+        let result = ""
+        let result2 = `Table of => (${j}) ` 
+        for (let i = 1; i < 11; i++) {
+                result = `${i}x${j} = ${i*j} |`
+                result2 += result
+        }
+        console.log(result2)
+        }
